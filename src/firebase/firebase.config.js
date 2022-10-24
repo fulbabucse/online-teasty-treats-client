45063bbbe362e13343a3cd3c9151b8a22c6f74f2
Fulbabu <fulbabucse@gmail.com>
2022-10-24 16:42:17 +0600

    firebase deploy

--- src/firebase/firebase.config.js
+++ src/firebase/firebase.config.js
@@ -1,19 +1,13 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyDjkjwwgfhqVWBWWkfFggvTvDiwAptJ7hE",
-  authDomain: "online-tasty-treats.firebaseapp.com",
-  projectId: "online-tasty-treats",
-  storageBucket: "online-tasty-treats.appspot.com",
-  messagingSenderId: "538445351279",
-  appId: "1:538445351279:web:a245c582ed7f2fabf7c09a",
+  apiKey: process.env.REACT_APP_apiKey,
+  authDomain: process.env.REACT_APP_authDomain,
+  projectId: process.env.REACT_APP_projectId,
+  storageBucket: process.env.REACT_APP_storageBucket,
+  messagingSenderId: process.env.REACT_APP_messagingSenderId,
+  appId: process.env.REACT_APP_appId,
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 
 export default app;
